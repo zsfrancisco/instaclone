df119f821b1521a7391be14714b1be6452f42985
Francisco Zambrano <franciscozambrano@outlook.es>
2021-04-05 21:16:09 -0500

    feat: Login functionality

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,65 +3,65 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 function createToken(user, SECRET_KEY, expiresIn) {
-    const { id, name, email, username } = user;
-    const payload = {
-        id,
-        name,
-        email,
-        username,
-    };
-    return jwt.sign(payload, SECRET_KEY, { expiresIn });
+  const { id, name, email, username } = user;
+  const payload = {
+    id,
+    name,
+    email,
+    username,
+  };
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
 }
 
 async function register(input) {
-    const newUser = input;
-    newUser.email = newUser.email.toLowerCase();
-    newUser.username = newUser.username.toLowerCase();
+  const newUser = input;
+  newUser.email = newUser.email.toLowerCase();
+  newUser.username = newUser.username.toLowerCase();
 
-    const { email, username, password } = newUser;
+  const { email, username, password } = newUser;
 
-    // Checking if email is in use
-    const emailFound = await User.findOne({ email });
-    if (emailFound) throw new Error("El correo electrónico ya está en uso");
+  // Checking if email is in use
+  const emailFound = await User.findOne({ email });
+  if (emailFound) throw new Error("El correo electrónico ya está en uso");
 
-    // Checking if email is in use
-    const usernameFound = await User.findOne({ username });
-    if (usernameFound) throw new Error("El nombre de usuario ya está en uso");
+  // Checking if email is in use
+  const usernameFound = await User.findOne({ username });
+  if (usernameFound) throw new Error("El nombre de usuario ya está en uso");
 
-    // Encrypting password
-    const salt = await bcryptjs.genSaltSync(10);
-    newUser.password = await bcryptjs.hash(password, salt);
+  // Encrypting password
+  const salt = await bcryptjs.genSaltSync(10);
+  newUser.password = await bcryptjs.hash(password, salt);
 
-    try {
-        const user = new User(newUser);
-        user.save();
-        return user;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const user = new User(newUser);
+    user.save();
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function getUser() {
-    console.log("Getting user");
-    return null;
+  console.log("Getting user");
+  return null;
 }
 
 async function login(input) {
-    const { email, password } = input;
+  const { email, password } = input;
 
-    const userFound = await User.findOne({ email: email.toLowerCase() });
-    if (!userFound) throw new Error("Error en el email o la contraseña");
+  const userFound = await User.findOne({ email: email.toLowerCase() });
+  if (!userFound) throw new Error("Error en el email o la contraseña");
 
-    const passwordSuccess = await bcryptjs.compare(password, userFound.password);
-    if (!passwordSuccess) throw new Error("Error en el email o la contraseña");
+  const passwordSuccess = await bcryptjs.compare(password, userFound.password);
+  if (!passwordSuccess) throw new Error("Error en el email o la contraseña");
 
-    return {
-        token: createToken(userFound, process.env.SECRET_KEY, "24h"),
-    };
+  return {
+    token: createToken(userFound, process.env.SECRET_KEY, "24h"),
+  };
 }
 
 module.exports = {
-    getUser,
-    register,
-    login,
+  getUser,
+  register,
+  login,
 };
