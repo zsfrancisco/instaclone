7f732b6e73e6ec8e94f4f2b62612ff8a88fb09d1
Francisco Zambrano <franciscozambrano@outlook.es>
2021-04-05 21:41:33 -0500

    feat: SASS added and connecting client with server using apollo-client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import { Button } from "semantic-ui-react";
+import { ApolloProvider } from "@apollo/client";
+import client from "./config/apollo";
 
 export default function App() {
   return (
-    <div className="app">
-      <Button primary>Primary</Button>
-      <Button secondary>Secondary</Button>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="app">
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+      </div>
+    </ApolloProvider>
   );
 }
