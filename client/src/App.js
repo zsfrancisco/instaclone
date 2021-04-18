d7c8d7a7ecb2925e37c2aa6ba849ae27d5a1135b
Francisco Zambrano <franciscozambrano@outlook.es>
2021-04-17 22:02:40 -0500

    feat: register form added - visual

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { Button } from "semantic-ui-react";
 import { ApolloProvider } from "@apollo/client";
 import client from "./config/apollo";
 import Auth from "./pages/Auth";
 
 export default function App() {
-  const [auth, setAuth] = useState({ name: "Francisco " });
+  const [auth, setAuth] = useState(undefined);
 
   return (
     <ApolloProvider client={client}>
--- client/src/pages/Auth/Auth.js
+++ client/src/pages/Auth/Auth.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Image } from "semantic-ui-react";
+import RegisterForm from "../../components/Auth/RegisterForm";
+import instaclone from "../../assets/png/instaclone.png";
 import "./Auth.scss";
 
 export default function Auth() {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
-    <div>
-      <h1>Estamos en auth</h1>
-    </div>
+    <Container fluid className="auth">
+      <Image src={instaclone} />
+
+      <div className="container-form">
+        {showLogin ? (
+          <p>Formulario de login</p>
+        ) : (
+          <RegisterForm setShowLogin={setShowLogin} />
+        )}
+      </div>
+
+      <div className="change-form">
+        <p>
+          {showLogin ? (
+            <>
+              ¿No tienes cuenta?
+              <span onClick={() => setShowLogin(!showLogin)}>Regístrate</span>
+            </>
+          ) : (
+            <>
+              ¡Entra con tu cuenta!
+              <span onClick={() => setShowLogin(!showLogin)}>
+                Iniciar sesión
+              </span>
+            </>
+          )}
+        </p>
+      </div>
+    </Container>
   );
 }
