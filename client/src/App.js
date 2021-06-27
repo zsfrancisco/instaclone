d784fec54ccefeba97d94f553a8cedc0356a18aa
Francisco Zambrano <franciscozambrano@outlook.es>
2021-06-26 20:28:06 -0500

    feat: add register funcionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { ApolloProvider } from "@apollo/client";
+import { ToastContainer } from "react-toastify";
 import client from "./config/apollo";
 import Auth from "./pages/Auth";
 
@@ -9,6 +10,17 @@ export default function App() {
   return (
     <ApolloProvider client={client}>
       {!auth ? <Auth /> : <h1>Estas logueado</h1>}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </ApolloProvider>
   );
 }
---
+++ client/src/gql/user.js
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client";
+
+export const REGISTER = gql`
+  mutation register($input: UserInput) {
+    register(input: $input) {
+      id
+      name
+      username
+      email
+      createAt
+    }
+  }
+`;
